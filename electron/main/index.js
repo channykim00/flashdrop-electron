574d4b1dbf8c897e23722beabde6c40a2e71fb46
ben0429 <hadeksim0429@gmail.com>
2026-04-09 16:46:23 +0900

    refactor: 소켓 핸들러 분리

--- electron/main/index.js
+++ electron/main/index.js
@@ -2,45 +2,23 @@ import fs from "fs";
 import path from "path";
 
 import { app, ipcMain, dialog, shell } from "electron";
-import { io } from "socket.io-client";
 
-import { API_URL } from "../config/constants.js";
 import downloadStore from "../utils/downloadStore.js";
 import linkStore from "../utils/linkStore.js";
 import startFileSync from "../utils/startFileSync.js";
 import uploadRequestStore from "../utils/uploadRequestStore.js";
 
 import { getOrCreateDeviceId } from "./deviceId.js";
-import { handleChunkReceive } from "./handlers/fileReceiver.js";
 import { createAppMenu } from "./menu.js";
+import { setupSocket } from "./socket/index.js";
 import { createMainWindow } from "./windows/mainWindow.js";
 
 app.whenReady().then(() => {
   const mainWindow = createMainWindow();
   createAppMenu();
 
-  const socket = io(API_URL);
+  const socket = setupSocket(mainWindow);
 
-  socket.on("connect_error", (err) => {
-    console.error("소켓 연결 오류:", err.message);
-  });
-
-  socket.on("receive-chunk", (data) => {
-    handleChunkReceive(data, socket);
-  });
-
-  socket.on("request-upload-accept", async (data) => {
-    if (data.autoAccept === true) {
-      mainWindow.webContents.send("auto-accept-upload", data);
-      socket.emit("accept-upload", { uploadData: data });
-      return;
-    }
-    const requests = uploadRequestStore.get("uploadRequests") || [];
-    requests.push(data);
-    uploadRequestStore.set("uploadRequests", requests);
-
-    mainWindow.webContents.send("show-upload-accept", data);
-  });
   ipcMain.on("accept-upload", (event, { uploadData }) => {
     socket.emit("accept-upload", { uploadData });
   });
